4d2c01750cd1baec76bcf676604dbe1dd2020580
Vignesh <vignesh77024@gmail.com>
2024-07-16 19:20:28 +0530

    WIP: implimenting angle constraints

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ let circles: Circle[] = []
 
 const gapBetweenCircles = 45
 const radiusOfCircle = 10
+const numberOfCircles = 5
 
 function init() {
   updateCirclePositions(circles)
@@ -37,10 +38,24 @@ function distanceBetweenPoints(
   return Math.sqrt(x * x + y * y)
 }
 
+function findAngle(
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+  x3: number,
+  y3: number
+) {
+  let a = Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2),
+    b = Math.pow(x2 - x3, 2) + Math.pow(y2 - y3, 2),
+    c = Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2)
+  return Math.acos((a + b - c) / Math.sqrt(4 * a * b))
+}
+
 function updateCirclePositions(circles: Circle[]) {
   // initialising circles
   if (circles.length == 0) {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < numberOfCircles; i++) {
       const circle = new Circle(
         currentMousePosition.x + gapBetweenCircles * i,
         currentMousePosition.y,
@@ -52,7 +67,7 @@ function updateCirclePositions(circles: Circle[]) {
   }
 
   //updating circles
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < numberOfCircles; i++) {
     if (i == 0) {
       circles[i].x = currentMousePosition.x
       circles[i].y = currentMousePosition.y
@@ -60,28 +75,40 @@ function updateCirclePositions(circles: Circle[]) {
       continue
     }
 
-    /// need to add stuff here
-
-    // check distance between current circle and the one before
-    // positions of both circles
     const x1 = circles[i - 1].x
     const y1 = circles[i - 1].y
     const x2 = circles[i].x
     const y2 = circles[i].y
+
+    /* DISTANCE CONSTRAINT */
+    // check distance between current circle and the one before
+    // positions of both circles
     const distance = distanceBetweenPoints({ x1, y1 }, { x2, y2 })
 
     // console.log(distance)
-    if (distance > gapBetweenCircles) {
-      console.log("out of range")
+    // if (distance > gapBetweenCircles) {
+    // console.log("out of range")
 
-      const ratioOfDistances = gapBetweenCircles / distance
+    const ratioOfDistances = gapBetweenCircles / distance
 
-      // calculating the required point that is in range of the prev circle
+    // calculating the required point that is in range of the prev circle
 
-      const x3 = (1 - ratioOfDistances) * x1 + ratioOfDistances * x2
-      const y3 = (1 - ratioOfDistances) * y1 + ratioOfDistances * y2
-      circles[i].x = x3
-      circles[i].y = y3
+    const newPositionX = (1 - ratioOfDistances) * x1 + ratioOfDistances * x2
+    const newPositionY = (1 - ratioOfDistances) * y1 + ratioOfDistances * y2
+    circles[i].x = newPositionX
+    circles[i].y = newPositionY
+    // }
+    /* ANGLE CONSTRAINT */
+    if (i < circles.length - 1) {
+      // co-ords to the next circle
+      const x3 = circles[i + 1].x
+      const y3 = circles[i + 1].y
+
+      // angle b/w the previous point to the current
+      // const angleWithPreviousCircle = Math.atan2(dy, dx)
+      const angleWithNextCircle =
+        (findAngle(x1, y1, x2, y2, x3, y3) * 180) / Math.PI
+      if (i == 1) console.log(angleWithNextCircle)
     }
   }
 }
@@ -118,7 +145,7 @@ function draw() {
   if (!context) return
 
   // filling the background
-  context.fillStyle = "brown"
+  context.fillStyle = "#222222"
   context.fillRect(0, 0, 1280, 720)
   // console.log(currentMousePosition)
 
