ef652eb175ab1f04da0701edf7682e9dd2fdbe55
Vignesh <vignesh77024@gmail.com>
2024-07-20 12:33:10 +0530

    WIP angle constraints

--- src/main.ts
+++ src/main.ts
@@ -5,53 +5,107 @@ window.addEventListener("mousemove", function (event) {
   getMousePosition(event, canvasElement)
 })
 
-let currentMousePosition = { x: 0, y: 0 }
+let currentMousePosition: Point = { x: 0, y: 0 }
 
 class Circle {
-  x: number
-  y: number
+  position: Point
   radius: number
   constructor(x: number, y: number, radius: number) {
-    this.x = x
-    this.y = y
+    this.position = { x, y }
+    // this.position.y = y
     this.radius = radius
   }
 }
-
+interface Point {
+  x: number
+  y: number
+}
 let circles: Circle[] = []
 
 const gapBetweenCircles = 45
 const radiusOfCircle = 10
-const numberOfCircles = 5
+const numberOfCircles = 3
 
 function init() {
   updateCirclePositions(circles)
   window.requestAnimationFrame(draw)
 }
 
-function distanceBetweenPoints(
-  { x1, y1 }: { x1: number; y1: number },
-  { x2, y2 }: { x2: number; y2: number }
-): number {
-  const x = x2 - x1
-  const y = y2 - y1
+function distanceBetweenPoints(pointA: Point, pointB: Point): number {
+  const x = pointB.x - pointA.x
+  const y = pointB.y - pointA.y
   return Math.sqrt(x * x + y * y)
 }
 
-function findAngle(
-  x1: number,
-  y1: number,
-  x2: number,
-  y2: number,
-  x3: number,
-  y3: number
-) {
-  let a = Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2),
-    b = Math.pow(x2 - x3, 2) + Math.pow(y2 - y3, 2),
-    c = Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2)
-  return Math.acos((a + b - c) / Math.sqrt(4 * a * b))
+// function findAngle(
+//   x1: number,
+//   y1: number,
+//   x2: number,
+//   y2: number,
+//   x3: number,
+//   y3: number
+// ) {
+//   let a = Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2),
+//     b = Math.pow(x2 - x3, 2) + Math.pow(y2 - y3, 2),
+//     c = Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2)
+//   return Math.acos((a + b - c) / Math.sqrt(4 * a * b))
+// }
+function calculateAngleFromXAxis(pointA: Point, pointB: Point): number {
+  return (Math.atan2(pointB.y - pointA.y, pointB.x - pointA.x) * 180) / Math.PI
+}
+function calculateAngle(A: Point, B: Point, C: Point) {
+  // Vectors AB and BC
+  const AB = { x: B.x - A.x, y: B.y - A.y }
+  const BC = { x: C.x - B.x, y: C.y - B.y }
+
+  // Dot product of AB and BC
+  const dotProduct = AB.x * BC.x + AB.y * BC.y
+
+  // Magnitudes of AB and BC
+  const magnitudeAB = Math.sqrt(AB.x ** 2 + AB.y ** 2)
+  const magnitudeBC = Math.sqrt(BC.x ** 2 + BC.y ** 2)
+
+  // Cosine of the angle
+  const cosTheta = dotProduct / (magnitudeAB * magnitudeBC)
+
+  // Angle in radians
+  const angleRadians = Math.acos(cosTheta)
+
+  // Convert angle to degrees
+  const angleDegrees = angleRadians * (180 / Math.PI)
+
+  // let angleDegreesDirectional = angleDegrees
+  // if (dotProduct < 0) angleDegreesDirectional *= -1
+
+  return angleDegrees
 }
 
+// function rotatePoint(point: Point, center: Point, angle: number) {
+//   const radians = angle * (Math.PI / 180)
+//   const cos = Math.cos(radians)
+//   const sin = Math.sin(radians)
+
+//   const translatedX = point.x - center.x
+//   const translatedY = point.y - center.y
+
+//   const rotatedX = translatedX * cos - translatedY * sin + center.x
+//   const rotatedY = translatedX * sin + translatedY * cos + center.y
+
+//   return { x: rotatedX, y: rotatedY }
+// }
+function rotatePoint(point: Point, center: Point, angle: number) {
+  let radians = angle * (Math.PI / 180) // Convert to radians
+  let cos = Math.cos(radians)
+  let sin = Math.sin(radians)
+
+  let x = cos * (point.x - center.x) - sin * (point.y - center.y) + center.x
+  let y = sin * (point.x - center.x) + cos * (point.y - center.y) + center.y
+
+  return { x: x, y: y }
+}
+function normalizeAngle(angle: number) {
+  return ((angle % 360) + 360) % 360
+}
 function updateCirclePositions(circles: Circle[]) {
   // initialising circles
   if (circles.length == 0) {
@@ -69,21 +123,28 @@ function updateCirclePositions(circles: Circle[]) {
   //updating circles
   for (let i = 0; i < numberOfCircles; i++) {
     if (i == 0) {
-      circles[i].x = currentMousePosition.x
-      circles[i].y = currentMousePosition.y
+      circles[i].position.x = currentMousePosition.x
+      circles[i].position.y = currentMousePosition.y
       // console.log("updated circle 0")
       continue
     }
 
-    const x1 = circles[i - 1].x
-    const y1 = circles[i - 1].y
-    const x2 = circles[i].x
-    const y2 = circles[i].y
-
+    const x1 = circles[i - 1].position.x
+    const y1 = circles[i - 1].position.y
+    const previousPoint: Point = {
+      x: circles[i - 1].position.x,
+      y: circles[i - 1].position.y,
+    }
+    const x2 = circles[i].position.x
+    const y2 = circles[i].position.y
+    const currentPoint: Point = {
+      x: circles[i].position.x,
+      y: circles[i].position.y,
+    }
     /* DISTANCE CONSTRAINT */
     // check distance between current circle and the one before
     // positions of both circles
-    const distance = distanceBetweenPoints({ x1, y1 }, { x2, y2 })
+    const distance = distanceBetweenPoints({ x: x1, y: y1 }, { x: x2, y: y2 })
 
     // console.log(distance)
     // if (distance > gapBetweenCircles) {
@@ -95,20 +156,107 @@ function updateCirclePositions(circles: Circle[]) {
 
     const newPositionX = (1 - ratioOfDistances) * x1 + ratioOfDistances * x2
     const newPositionY = (1 - ratioOfDistances) * y1 + ratioOfDistances * y2
-    circles[i].x = newPositionX
-    circles[i].y = newPositionY
+    circles[i].position.x = newPositionX
+    circles[i].position.y = newPositionY
     // }
     /* ANGLE CONSTRAINT */
     if (i < circles.length - 1) {
       // co-ords to the next circle
-      const x3 = circles[i + 1].x
-      const y3 = circles[i + 1].y
+      const x3 = circles[i + 1].position.x
+      const y3 = circles[i + 1].position.y
+      const nextPoint: Point = {
+        x: circles[i + 1].position.x,
+        y: circles[i + 1].position.y,
+      }
 
+      //////////////////////////////////////////
+      // if (i == 1) {
+      // the above "if" is here for testing purposes, otherwise the logs will be flooded
+
+      // check for angle
+      const angle = calculateAngle(previousPoint, currentPoint, nextPoint)
+      // console.log(angle)
+      const parentLineAngle = calculateAngleFromXAxis(
+        previousPoint,
+        currentPoint
+      )
+
+      // "Normal" is the angle normalized to 0 to 360 instead of -180 to 180
+      const parentLineAngleNormal =
+        parentLineAngle < 0
+          ? 180 + (180 - parentLineAngle * Math.sign(parentLineAngle))
+          : parentLineAngle
+      const childLineAngle = calculateAngleFromXAxis(currentPoint, nextPoint)
+      const childLineAngleNormal =
+        childLineAngle < 0
+          ? 180 + (180 - childLineAngle * Math.sign(childLineAngle))
+          : childLineAngle
+      let parentChildNormalDiff = parentLineAngleNormal - childLineAngleNormal
+      let parentChildDiff = parentLineAngle - childLineAngle
+      // determines the direction of the rotation
+      // if (parentLineAngle > childLineAngle) {
+      //   rotationDirection = "clockwise"
+      // }
+      let rotationDirection = -1
+      if (parentLineAngle >= childLineAngle) {
+        rotationDirection = 1
+      }
+      // let rotationAngle =
+      //   Math.sign(90 - angle) * (90 - angle) * rotationDirection
+      console.log({
+        // parentLineAngleNormal,
+        // childLineAngleNormal,
+        // parentLineAngle,
+        // childLineAngle,
+        parentChildNormalDiff,
+        parentChildDiff,
+        // angle,
+        // rotationDirection,
+        // rotationAngle: -(90 - angle) * rotationDirection,
+        // rotationAngle: -(90 - angleDiff) * rotationDirection,
+      })
+      if (angle > 90) {
+        const rotationAngle = (angle - 90) * rotationDirection
+        // const rotationAngle = 30 * rotationDirection
+        // if (rotationAngle > 45) {
+        // console.warn({
+        //   parentLineAngleNormal,
+        //   childLineAngleNormal,
+        //   angle,
+        //   rotationAngle,
+        //   rotationDirection,
+        // })
+        // }
+        // const rotatedPoint = rotatePoint(
+        //   nextPoint,
+        //   currentPoint,
+        //   rotationAngle
+        //   // -(90 - angleDiff) * rotationDirection
+        // )
+        // circles[i + 1].position.x = rotatedPoint.x
+        // circles[i + 1].position.y = rotatedPoint.y
+      }
+      // }
+
+      /////////////////////////////////////////
       // angle b/w the previous point to the current
       // const angleWithPreviousCircle = Math.atan2(dy, dx)
-      const angleWithNextCircle =
-        (findAngle(x1, y1, x2, y2, x3, y3) * 180) / Math.PI
-      if (i == 1) console.log(angleWithNextCircle)
+      // const angleWithNextCircle =
+      // (findAngle(x1, y1, x2, y2, x3, y3) * 180) / Math.PI
+
+      ///////////////////////////////
+      // if (angleWithNextCircle < 90) {
+      //   console.log(angleWithNextCircle)
+      //   circles[i + 1].x =
+      //     newPositionX +
+      //     (radiusOfCircle + gapBetweenCircles) * Math.cos(Math.PI / 2)
+      //   circles[i + 1].y =
+      //     newPositionY +
+      //     (radiusOfCircle + gapBetweenCircles) * Math.sin(Math.PI / 2)
+      // }
+      // if (i == 1) console.log(angleWithNextCircle)
+
+      // x = cos(a)  ; y = sin(a)
     }
   }
 }
@@ -131,7 +279,13 @@ updateCirclePositions(circles)
 function drawCircles(context: CanvasRenderingContext2D, circles: Circle[]) {
   circles.forEach((circle) => {
     context.beginPath()
-    context.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI)
+    context.arc(
+      circle.position.x,
+      circle.position.y,
+      circle.radius,
+      0,
+      2 * Math.PI
+    )
     context.fillStyle = "red"
     context.fill()
   })
